361a1f6e11e731b1465c13f9c8e83b14ff38bc6e
vlad <vladstepanenko1@gmail.com>
2019-04-26 16:24:32 +0300

    Add teams component

--- RecommendationApp-Client/src/app/app.module.ts
+++ RecommendationApp-Client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
