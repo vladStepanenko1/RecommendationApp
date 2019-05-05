1c62373b7c9dcd62e549ef036b376947e659c9ae
vlad <vladstepanenko1@gmail.com>
2019-05-05 11:37:47 +0300

    Add UserParams class

---
+++ RecommendationApp.API/Helpers/UserParams.cs
@@ -0,0 +1,20 @@
+namespace RecommendationApp.API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 25;
+        private int pageSize = 10;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize 
+        { 
+            get
+            {
+                return pageSize;
+            }
+            set
+            {
+                pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+            }
+        }
+    }
+}
